17b8d60506006c8dcb55608df27a31ec19ff1d62
manishapariyar <manishapariyar196@gmail.com>
2024-01-08 15:15:06 +0545

    adding new api web

--- weatherinfo/script.js
+++ weatherinfo/script.js
@@ -7,13 +7,15 @@ const humidity = document.querySelector(`#humidity`);
 const wind_speed = document.querySelector(`#wind_speed`);
 
   async function checkweather(city){
-    const api_key= "48c25d6cf9461915299e317c93c72030"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city }&appid=${api_key}`;
+    const api_key= "4817d016cdfe271617c26e9842fee6df"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
     const weather_data =  await fetch(`${url}`).then(response=>response.json());
     console.log(weather_data)
+  
 
 }
-btn,addEventListener('click', ()=>{
-    checkweather(inputbox.value);
-});
+// btn.addEventListener('click', ()=>{
+    
+// });
 
+checkweather();
